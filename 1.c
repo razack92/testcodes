bf15c9c476912028feded29031b4f373c7ffa6ca
razack92 <abdulrazackn92@gmail.com>
2021-11-12 14:59:40 +0530

    first file

---
+++ 1.c
@@ -0,0 +1,13 @@
+//print 1 to 100 without using conditional operator
+
+#include<stdio.h>
+
+void main()
+{
+	unsigned short int a=1,b=100;
+
+	while(b--)
+		printf("%2d\t",a++);
+
+	printf("\n");
+}	
